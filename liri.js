ffaeb224c02371e20e256cf61d6e1c64b23ccd32
sbstiane46 <sbstian89@yahoo.com>
2019-06-27 21:54:00 -0400

    progress

--- liri.js
+++ liri.js
@@ -6,3 +6,17 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 
+ 
+var spotify = new Spotify({
+  id: <b7cc374412f442fb95acb7d20ad572d3>,
+  secret: <b27c83425d394f789ed8fda5a1542322>
+});
+ 
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
