a3dea7957f191e09401074d2dbc1f363201c1e31
sbstiane46 <sbstian89@yahoo.com>
2019-08-22 15:40:13 -0400

    progress

--- keys.js
+++ keys.js
@@ -4,3 +4,9 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+exports.omdb = {
+  id: process.env.OMDB_apikey    
+};
+exports.bandsInTown = {
+  id: process.env.BANDSinTOWN_app_id    
+};
--- liri.js
+++ liri.js
@@ -54,7 +54,7 @@ function start_liri() {
             } else {
                 console.log("You get this");
 
-                fs.readFile("random.txt" , "uft8", funciton (error, data) {
+                fs.readFile("random.txt" , "uft8", function (error, data) {
                     if (error) {
                         console.log(error);
                     }
@@ -171,6 +171,78 @@ function start_liri() {
             })
         )
     }
+
+    function search_songs(songName) {
+        if (songName === "") {
+            songName = "Bohemian Rapsody";
+        }
+        spotify.search({ type: 'track', query: songName }, function (err, data) {
+            if (err) {return console.log('Error occurred: ' + err);}
+            for (var i = 0; i < data.tracks.items.length; i++) {
+
+                let header = "***** SONG *****";
+                let track = "NAME: " + data.tracks.items[i].name;
+                let artist = "ARTIST: " + data.tracks.items[i].artist[0].name;
+                let album = "ALBUM: " + data.tracks.items[i].album.name;
+                let release = "RELEASE: " + data.tracks.items[i].album.release_date;
+                let preview = "PREVIEW: " + data.tracks.items[i].external_urls.spotify;
+
+                console.log(header);
+                console.log(track);
+                console.log(artist);
+                console.log(album);
+                console.log(release);
+                console.log(preview);
+                console.log("");
+                append_log(header);
+                append_log(track);
+                append_log(artist);
+                append_log(album);
+                append_log(release);
+                append_log(preview);
+
+                console.log("");
+                setTimeout(start_liri, 2000);
+            }
+        });
+    }
+    function append_log(appendText){
+        fs.appendFileSync("log.txt", appendText + "\n");
+    }
+
+//     let header = "********** SONG **********";
+//     let track = "NAME:    " + data.tracks.items[i].name;
+//     let artist = "ARTIST:  " + data.tracks.items[i].artists[0].name;
+//     let album = "ALBUM:   " + data.tracks.items[i].album.name;
+//     let release = "RELEASE: " + data.tracks.items[i].album.release_date;
+//     let preview = "PREVIEW: " + data.tracks.items[i].external_urls.spotify;
+
+//     console.log(header);
+//     console.log(track);
+//     console.log(artist);
+//     console.log(album);
+//     console.log(release);
+//     console.log(preview);
+//     console.log("");
+
+//     append_log(header);
+//     append_log(track);
+//     append_log(artist);
+//     append_log(album);
+//     append_log(release);
+//     append_log(preview);
+// }
+
+// console.log("");
+// setTimeout(start_liri, 4000);
+
+// });
+// }
+// function append_log(appendText) {
+
+// fs.appendFileSync("log.txt", appendText + "\n");
+
+// }
 
     
  
